3cb3be087af6bba890c9e30cfd10c09f702d9e7c
gabrielhso <gabriel.onishi@outlook.com>
2023-11-04 19:31:33 -0300

    Inicia relatório das soluções

--- scripts/generate_graph.py
+++ scripts/generate_graph.py
@@ -1,13 +1,16 @@
 import networkx as nx
 import sys, os
 
+script_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(script_directory)
+
 num_vertices = int(sys.argv[1])
 connection_probability = float(sys.argv[2])
 graph_name = sys.argv[3].split('.')[0]
 
 graph = nx.fast_gnp_random_graph(num_vertices, connection_probability)
 
-output_directory = os.path.join(os.pardir, "graphs")
+output_directory = os.path.join(parent_directory, "graphs")
 os.makedirs(output_directory, exist_ok=True)
 
 filename = os.path.join(output_directory, f"{graph_name}.txt")
@@ -17,4 +20,4 @@ with open(filename, 'w') as f:
     for edge in graph.edges():
         f.write(f"{edge[0]+1} {edge[1]+1}\n")
 
-print(f"Grafo densamente conectado gerado e salvo em ../graphs/{filename}'.")
+print(f"Grafo densamente conectado gerado e salvo em {parent_directory}/graphs'.")
